qt/qtquick1

commit 5e970b84663f5398eb51d4575856d1a3c44df953

Fix assert when binding to property variant.
Remove workarounds for the QVariant meta-type and use it directly and
remove QMetaObjectBuilder entirely and use the up to date copy in
qtbase.

Change-Id: I1ad601906d6b172adc7ce7bb63af28cd578eb5d7
Reviewed-by: Andrew den Exter <andrew.den-exter@nokia.com>

src/declarative/qml/qdeclarativecompiler.cpp
@@ -196,7 +196,7 @@ bool QDeclarativeCompiler::testLiteralAssignment(const QMetaProperty &prop,
     }
     int type = prop.userType();
     switch(type) {
-        case -1:
+        case QMetaType::QVariant:
             break;
         case QVariant::String:
             if (!v->value.isString()) COMPILE_EXCEPTION(v, tr("Invalid property assignment: string expected"));
@@ -340,7 +340,7 @@ void QDeclarativeCompiler::genLiteralAssignment(const QMetaProperty &prop,
 
     int type = prop.userType();
     switch(type) {
-        case -1:
+        case QMetaType::QVariant:
             {
             if (v->value.isNumber()) {
                 double n = v->value.asNumber();
@@ -1145,7 +1145,7 @@ void QDeclarativeCompiler::genValueTypeProperty(QDeclarativeParser::Object *obj,
     fetch.fetchValue.bindingSkipList = 0;
     fetch.line = prop->location.start.line;
 
-    if (obj->type == -1 || output->types.at(obj->type).component) {
+    if (obj->type == QMetaType::QVariant || output->types.at(obj->type).component) {
         // We only have to do this if this is a composite type.  If it is a builtin
         // type it can't possibly already have bindings that need to be cleared.
         foreach(Property *vprop, prop->value->valueProperties) {
@@ -1675,7 +1675,7 @@ void QDeclarativeCompiler::genPropertyAssignment(QDeclarativeParser::Property *p
                 store.storeObject.propertyIndex = prop->index;
                 output->bytecode << store;
 
-            } else if (prop->type == -1) {
+            } else if (prop->type == QMetaType::QVariant) {
 
                 QDeclarativeInstruction store;
                 store.type = QDeclarativeInstruction::StoreVariantObject;
@@ -2064,7 +2064,7 @@ bool QDeclarativeCompiler::buildPropertyObjectAssignment(QDeclarativeParser::Pro
 
         v->type = Value::CreatedObject;
 
-    } else if (prop->type == -1) {
+    } else if (prop->type == QMetaType::QVariant) {
 
         // Assigning an object to a QVariant
         COMPILE_CHECK(buildObject(v->object, ctxt));
@@ -2491,7 +2491,7 @@ bool QDeclarativeCompiler::buildDynamicMeta(QDeclarativeParser::Object *obj, Dyn
             }
             break;
         case Object::DynamicProperty::Variant:
-            propertyType = -1;
+            propertyType = QMetaType::QVariant;
             type = "QVariant";
             break;
         case Object::DynamicProperty::Int:

src/declarative/qml/qdeclarativeproperty.cpp
@@ -444,10 +444,7 @@ int QDeclarativePropertyPrivate::propertyType() const
     if (isValueType()) {
         return valueType.valueTypePropType;
     } else if (type & QDeclarativeProperty::Property) {
-        if (core.propType == (int)QVariant::LastType)
-            return qMetaTypeId<QVariant>();
-        else
-            return core.propType;
+        return core.propType;
     } else {
         return QVariant::Invalid;
     }

src/declarative/qml/qdeclarativepropertycache.cpp
@@ -83,8 +83,6 @@ QDeclarativePropertyCache::Data::Flags QDeclarativePropertyCache::Data::flagsFor
 void QDeclarativePropertyCache::Data::load(const QMetaProperty &p, QDeclarativeEngine *engine)
 {
     propType = p.userType();
-    if (QVariant::Type(propType) == QVariant::LastType)
-        propType = qMetaTypeId<QVariant>();
     coreIndex = p.propertyIndex();
     notifyIndex = p.notifySignalIndex();
     flags = flagsForProperty(p, engine);

src/declarative/qml/qdeclarativevmemetaobject.cpp
@@ -463,99 +463,94 @@ int QDeclarativeVMEMetaObject::metaCall(QMetaObject::Call c, int _id, void **a)
                int t = (metaData->propertyData() + id)->propertyType;
                 bool needActivate = false;
 
-                if (t == -1) {
-
-                    if (c == QMetaObject::ReadProperty) {
+                if (c == QMetaObject::ReadProperty) {
+                    switch(t) {
+                    case QVariant::Int:
+                        *reinterpret_cast<int *>(a[0]) = data[id].asInt();
+                        break;
+                    case QVariant::Bool:
+                        *reinterpret_cast<bool *>(a[0]) = data[id].asBool();
+                        break;
+                    case QVariant::Double:
+                        *reinterpret_cast<double *>(a[0]) = data[id].asDouble();
+                        break;
+                    case QVariant::String:
+                        *reinterpret_cast<QString *>(a[0]) = data[id].asQString();
+                        break;
+                    case QVariant::Url:
+                        *reinterpret_cast<QUrl *>(a[0]) = data[id].asQUrl();
+                        break;
+                    case QVariant::Color:
+                        *reinterpret_cast<QColor *>(a[0]) = data[id].asQColor();
+                        break;
+                    case QVariant::Date:
+                        *reinterpret_cast<QDate *>(a[0]) = data[id].asQDate();
+                        break;
+                    case QVariant::DateTime:
+                        *reinterpret_cast<QDateTime *>(a[0]) = data[id].asQDateTime();
+                        break;
+                    case QMetaType::QObjectStar:
+                        *reinterpret_cast<QObject **>(a[0]) = data[id].asQObject();
+                        break;
+                    case QMetaType::QVariant:
                         *reinterpret_cast<QVariant *>(a[0]) = readVarPropertyAsVariant(id);
-                    } else if (c == QMetaObject::WriteProperty) {
-                        writeVarProperty(id, *reinterpret_cast<QVariant *>(a[0]));
+                        break;
+                    default:
+                        break;
+                    }
+                    if (t == qMetaTypeId<QDeclarativeListProperty<QObject> >()) {
+                        int listIndex = data[id].asInt();
+                        const List *list = &listProperties.at(listIndex);
+                        *reinterpret_cast<QDeclarativeListProperty<QObject> *>(a[0]) =
+                            QDeclarativeListProperty<QObject>(object, (void *)list,
+                                                              list_append, list_count, list_at,
+                                                              list_clear);
                     }
 
-                } else {
+                } else if (c == QMetaObject::WriteProperty) {
 
-                    if (c == QMetaObject::ReadProperty) {
-                        switch(t) {
-                        case QVariant::Int:
-                            *reinterpret_cast<int *>(a[0]) = data[id].asInt();
-                            break;
-                        case QVariant::Bool:
-                            *reinterpret_cast<bool *>(a[0]) = data[id].asBool();
-                            break;
-                        case QVariant::Double:
-                            *reinterpret_cast<double *>(a[0]) = data[id].asDouble();
-                            break;
-                        case QVariant::String:
-                            *reinterpret_cast<QString *>(a[0]) = data[id].asQString();
-                            break;
-                        case QVariant::Url:
-                            *reinterpret_cast<QUrl *>(a[0]) = data[id].asQUrl();
-                            break;
-                        case QVariant::Color:
-                            *reinterpret_cast<QColor *>(a[0]) = data[id].asQColor();
-                            break;
-                        case QVariant::Date:
-                            *reinterpret_cast<QDate *>(a[0]) = data[id].asQDate();
-                            break;
-                        case QVariant::DateTime:
-                            *reinterpret_cast<QDateTime *>(a[0]) = data[id].asQDateTime();
-                            break;
-                        case QMetaType::QObjectStar:
-                            *reinterpret_cast<QObject **>(a[0]) = data[id].asQObject();
-                            break;
-                        default:
-                            break;
-                        }
-                        if (t == qMetaTypeId<QDeclarativeListProperty<QObject> >()) {
-                            int listIndex = data[id].asInt();
-                            const List *list = &listProperties.at(listIndex);
-                            *reinterpret_cast<QDeclarativeListProperty<QObject> *>(a[0]) = 
-                                QDeclarativeListProperty<QObject>(object, (void *)list,
-                                                                  list_append, list_count, list_at, 
-                                                                  list_clear);
-                        }
-
-                    } else if (c == QMetaObject::WriteProperty) {
-
-                        switch(t) {
-                        case QVariant::Int:
-                            needActivate = *reinterpret_cast<int *>(a[0]) != data[id].asInt();
-                            data[id].setValue(*reinterpret_cast<int *>(a[0]));
-                            break;
-                        case QVariant::Bool:
-                            needActivate = *reinterpret_cast<bool *>(a[0]) != data[id].asBool();
-                            data[id].setValue(*reinterpret_cast<bool *>(a[0]));
-                            break;
-                        case QVariant::Double:
-                            needActivate = *reinterpret_cast<double *>(a[0]) != data[id].asDouble();
-                            data[id].setValue(*reinterpret_cast<double *>(a[0]));
-                            break;
-                        case QVariant::String:
-                            needActivate = *reinterpret_cast<QString *>(a[0]) != data[id].asQString();
-                            data[id].setValue(*reinterpret_cast<QString *>(a[0]));
-                            break;
-                        case QVariant::Url:
-                            needActivate = *reinterpret_cast<QUrl *>(a[0]) != data[id].asQUrl();
-                            data[id].setValue(*reinterpret_cast<QUrl *>(a[0]));
-                            break;
-                        case QVariant::Color:
-                            needActivate = *reinterpret_cast<QColor *>(a[0]) != data[id].asQColor();
-                            data[id].setValue(*reinterpret_cast<QColor *>(a[0]));
-                            break;
-                        case QVariant::Date:
-                            needActivate = *reinterpret_cast<QDate *>(a[0]) != data[id].asQDate();
-                            data[id].setValue(*reinterpret_cast<QDate *>(a[0]));
-                            break;
-                        case QVariant::DateTime:
-                            needActivate = *reinterpret_cast<QDateTime *>(a[0]) != data[id].asQDateTime();
-                            data[id].setValue(*reinterpret_cast<QDateTime *>(a[0]));
-                            break;
-                        case QMetaType::QObjectStar:
-                            needActivate = *reinterpret_cast<QObject **>(a[0]) != data[id].asQObject();
-                            data[id].setValue(*reinterpret_cast<QObject **>(a[0]));
-                            break;
-                        default:
-                            break;
-                        }
+                    switch(t) {
+                    case QVariant::Int:
+                        needActivate = *reinterpret_cast<int *>(a[0]) != data[id].asInt();
+                        data[id].setValue(*reinterpret_cast<int *>(a[0]));
+                        break;
+                    case QVariant::Bool:
+                        needActivate = *reinterpret_cast<bool *>(a[0]) != data[id].asBool();
+                        data[id].setValue(*reinterpret_cast<bool *>(a[0]));
+                        break;
+                    case QVariant::Double:
+                        needActivate = *reinterpret_cast<double *>(a[0]) != data[id].asDouble();
+                        data[id].setValue(*reinterpret_cast<double *>(a[0]));
+                        break;
+                    case QVariant::String:
+                        needActivate = *reinterpret_cast<QString *>(a[0]) != data[id].asQString();
+                        data[id].setValue(*reinterpret_cast<QString *>(a[0]));
+                        break;
+                    case QVariant::Url:
+                        needActivate = *reinterpret_cast<QUrl *>(a[0]) != data[id].asQUrl();
+                        data[id].setValue(*reinterpret_cast<QUrl *>(a[0]));
+                        break;
+                    case QVariant::Color:
+                        needActivate = *reinterpret_cast<QColor *>(a[0]) != data[id].asQColor();
+                        data[id].setValue(*reinterpret_cast<QColor *>(a[0]));
+                        break;
+                    case QVariant::Date:
+                        needActivate = *reinterpret_cast<QDate *>(a[0]) != data[id].asQDate();
+                        data[id].setValue(*reinterpret_cast<QDate *>(a[0]));
+                        break;
+                    case QVariant::DateTime:
+                        needActivate = *reinterpret_cast<QDateTime *>(a[0]) != data[id].asQDateTime();
+                        data[id].setValue(*reinterpret_cast<QDateTime *>(a[0]));
+                        break;
+                    case QMetaType::QObjectStar:
+                        needActivate = *reinterpret_cast<QObject **>(a[0]) != data[id].asQObject();
+                        data[id].setValue(*reinterpret_cast<QObject **>(a[0]));
+                        break;
+                    case QMetaType::QVariant:
+                        writeVarProperty(id, *reinterpret_cast<QVariant *>(a[0]));
+                        break;
+                    default:
+                        break;
                     }
 
                 }

src/declarative/qml/qmetaobjectbuilder_p.h
@@ -1,325 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the QtDeclarative module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** GNU Lesser General Public License Usage
-** This file may be used under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation and
-** appearing in the file LICENSE.LGPL included in the packaging of this
-** file. Please review the following information to ensure the GNU Lesser
-** General Public License version 2.1 requirements will be met:
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU General
-** Public License version 3.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of this
-** file. Please review the following information to ensure the GNU General
-** Public License version 3.0 requirements will be met:
-** http://www.gnu.org/copyleft/gpl.html.
-**
-** Other Usage
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
-**
-**
-**
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#ifndef QMETAOBJECTBUILDER_H
-#define QMETAOBJECTBUILDER_H
-
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists for the convenience
-// of moc.  This header file may change from version to version without notice,
-// or even be removed.
-//
-// We mean it.
-//
-
-#include <QtCore/qobject.h>
-#include <QtCore/qmetaobject.h>
-#include <QtCore/qdatastream.h>
-#include <QtCore/qmap.h>
-
-#include <private/qdeclarativeglobal_p.h>
-
-QT_BEGIN_NAMESPACE
-
-class QMetaObjectBuilderPrivate;
-class QMetaMethodBuilder;
-class QMetaMethodBuilderPrivate;
-class QMetaPropertyBuilder;
-class QMetaPropertyBuilderPrivate;
-class QMetaEnumBuilder;
-class QMetaEnumBuilderPrivate;
-
-class Q_DECLARATIVE_PRIVATE_EXPORT QMetaObjectBuilder
-{
-public:
-    enum AddMember
-    {
-        ClassName               = 0x00000001,
-        SuperClass              = 0x00000002,
-        Methods                 = 0x00000004,
-        Signals                 = 0x00000008,
-        Slots                   = 0x00000010,
-        Constructors            = 0x00000020,
-        Properties              = 0x00000040,
-        Enumerators             = 0x00000080,
-        ClassInfos              = 0x00000100,
-        RelatedMetaObjects      = 0x00000200,
-        StaticMetacall          = 0x00000400,
-        PublicMethods           = 0x00000800,
-        ProtectedMethods        = 0x00001000,
-        PrivateMethods          = 0x00002000,
-        AllMembers              = 0x7FFFFFFF,
-        AllPrimaryMembers       = 0x7FFFFBFC
-    };
-    Q_DECLARE_FLAGS(AddMembers, AddMember)
-
-    enum MetaObjectFlag {
-        DynamicMetaObject = 0x01
-    };
-    Q_DECLARE_FLAGS(MetaObjectFlags, MetaObjectFlag)
-
-    QMetaObjectBuilder();
-    explicit QMetaObjectBuilder(const QMetaObject *prototype, QMetaObjectBuilder::AddMembers members = AllMembers);
-    virtual ~QMetaObjectBuilder();
-
-    QByteArray className() const;
-    void setClassName(const QByteArray& name);
-
-    const QMetaObject *superClass() const;
-    void setSuperClass(const QMetaObject *meta);
-
-    MetaObjectFlags flags() const;
-    void setFlags(MetaObjectFlags);
-
-    int methodCount() const;
-    int constructorCount() const;
-    int propertyCount() const;
-    int enumeratorCount() const;
-    int classInfoCount() const;
-    int relatedMetaObjectCount() const;
-
-    QMetaMethodBuilder addMethod(const QByteArray& signature);
-    QMetaMethodBuilder addMethod(const QByteArray& signature, const QByteArray& returnType);
-    QMetaMethodBuilder addMethod(const QMetaMethod& prototype);
-
-    QMetaMethodBuilder addSlot(const QByteArray& signature);
-    QMetaMethodBuilder addSignal(const QByteArray& signature);
-
-    QMetaMethodBuilder addConstructor(const QByteArray& signature);
-    QMetaMethodBuilder addConstructor(const QMetaMethod& prototype);
-
-    QMetaPropertyBuilder addProperty(const QByteArray& name, const QByteArray& type, int notifierId=-1);
-    QMetaPropertyBuilder addProperty(const QMetaProperty& prototype);
-
-    QMetaEnumBuilder addEnumerator(const QByteArray& name);
-    QMetaEnumBuilder addEnumerator(const QMetaEnum& prototype);
-
-    int addClassInfo(const QByteArray& name, const QByteArray& value);
-
-#ifdef Q_NO_DATA_RELOCATION
-    int addRelatedMetaObject(const QMetaObjectAccessor &meta);
-#else
-    int addRelatedMetaObject(const QMetaObject *meta);
-#endif
-
-    void addMetaObject(const QMetaObject *prototype, QMetaObjectBuilder::AddMembers members = AllMembers);
-
-    QMetaMethodBuilder method(int index) const;
-    QMetaMethodBuilder constructor(int index) const;
-    QMetaPropertyBuilder property(int index) const;
-    QMetaEnumBuilder enumerator(int index) const;
-    const QMetaObject *relatedMetaObject(int index) const;
-
-    QByteArray classInfoName(int index) const;
-    QByteArray classInfoValue(int index) const;
-
-    void removeMethod(int index);
-    void removeConstructor(int index);
-    void removeProperty(int index);
-    void removeEnumerator(int index);
-    void removeClassInfo(int index);
-    void removeRelatedMetaObject(int index);
-
-    int indexOfMethod(const QByteArray& signature);
-    int indexOfSignal(const QByteArray& signature);
-    int indexOfSlot(const QByteArray& signature);
-    int indexOfConstructor(const QByteArray& signature);
-    int indexOfProperty(const QByteArray& name);
-    int indexOfEnumerator(const QByteArray& name);
-    int indexOfClassInfo(const QByteArray& name);
-
-    typedef QMetaObjectExtraData::StaticMetacallFunction StaticMetacallFunction;
-
-    QMetaObjectBuilder::StaticMetacallFunction staticMetacallFunction() const;
-    void setStaticMetacallFunction(QMetaObjectBuilder::StaticMetacallFunction value);
-
-    QMetaObject *toMetaObject() const;
-    QByteArray toRelocatableData(bool * = 0) const;
-    static void fromRelocatableData(QMetaObject *, const QMetaObject *, const QByteArray &);
-
-#ifndef QT_NO_DATASTREAM
-    void serialize(QDataStream& stream) const;
-    void deserialize
-        (QDataStream& stream,
-         const QMap<QByteArray, const QMetaObject *>& references);
-#endif
-
-private:
-    Q_DISABLE_COPY(QMetaObjectBuilder)
-
-    QMetaObjectBuilderPrivate *d;
-
-    friend class QMetaMethodBuilder;
-    friend class QMetaPropertyBuilder;
-    friend class QMetaEnumBuilder;
-};
-
-class Q_DECLARATIVE_PRIVATE_EXPORT QMetaMethodBuilder
-{
-public:
-    QMetaMethodBuilder() : _mobj(0), _index(0) {}
-
-    int index() const;
-
-    QMetaMethod::MethodType methodType() const;
-    QByteArray signature() const;
-
-    QByteArray returnType() const;
-    void setReturnType(const QByteArray& value);
-
-    QList<QByteArray> parameterNames() const;
-    void setParameterNames(const QList<QByteArray>& value);
-
-    QByteArray tag() const;
-    void setTag(const QByteArray& value);
-
-    QMetaMethod::Access access() const;
-    void setAccess(QMetaMethod::Access value);
-
-    int attributes() const;
-    void setAttributes(int value);
-
-private:
-    const QMetaObjectBuilder *_mobj;
-    int _index;
-
-    friend class QMetaObjectBuilder;
-    friend class QMetaPropertyBuilder;
-
-    QMetaMethodBuilder(const QMetaObjectBuilder *mobj, int index)
-        : _mobj(mobj), _index(index) {}
-
-    QMetaMethodBuilderPrivate *d_func() const;
-};
-
-class Q_DECLARATIVE_PRIVATE_EXPORT QMetaPropertyBuilder
-{
-public:
-    QMetaPropertyBuilder() : _mobj(0), _index(0) {}
-
-    int index() const { return _index; }
-
-    QByteArray name() const;
-    QByteArray type() const;
-
-    bool hasNotifySignal() const;
-    QMetaMethodBuilder notifySignal() const;
-    void setNotifySignal(const QMetaMethodBuilder& value);
-    void removeNotifySignal();
-
-    bool isReadable() const;
-    bool isWritable() const;
-    bool isResettable() const;
-    bool isDesignable() const;
-    bool isScriptable() const;
-    bool isStored() const;
-    bool isEditable() const;
-    bool isUser() const;
-    bool hasStdCppSet() const;
-    bool isEnumOrFlag() const;
-    bool isConstant() const;
-    bool isFinal() const;
-
-    void setReadable(bool value);
-    void setWritable(bool value);
-    void setResettable(bool value);
-    void setDesignable(bool value);
-    void setScriptable(bool value);
-    void setStored(bool value);
-    void setEditable(bool value);
-    void setUser(bool value);
-    void setStdCppSet(bool value);
-    void setEnumOrFlag(bool value);
-    void setConstant(bool value);
-    void setFinal(bool value);
-
-private:
-    const QMetaObjectBuilder *_mobj;
-    int _index;
-
-    friend class QMetaObjectBuilder;
-
-    QMetaPropertyBuilder(const QMetaObjectBuilder *mobj, int index)
-        : _mobj(mobj), _index(index) {}
-
-    QMetaPropertyBuilderPrivate *d_func() const;
-};
-
-class Q_DECLARATIVE_PRIVATE_EXPORT QMetaEnumBuilder
-{
-public:
-    QMetaEnumBuilder() : _mobj(0), _index(0) {}
-
-    int index() const { return _index; }
-
-    QByteArray name() const;
-
-    bool isFlag() const;
-    void setIsFlag(bool value);
-
-    int keyCount() const;
-    QByteArray key(int index) const;
-    int value(int index) const;
-
-    int addKey(const QByteArray& name, int value);
-    void removeKey(int index);
-
-private:
-    const QMetaObjectBuilder *_mobj;
-    int _index;
-
-    friend class QMetaObjectBuilder;
-
-    QMetaEnumBuilder(const QMetaObjectBuilder *mobj, int index)
-        : _mobj(mobj), _index(index) {}
-
-    QMetaEnumBuilderPrivate *d_func() const;
-};
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(QMetaObjectBuilder::AddMembers)
-Q_DECLARE_OPERATORS_FOR_FLAGS(QMetaObjectBuilder::MetaObjectFlags)
-
-QT_END_NAMESPACE
-
-#endif

src/declarative/qml/qml.pri
@@ -33,7 +33,6 @@ SOURCES += \
     $$PWD/qdeclarativefastproperties.cpp \
     $$PWD/qdeclarativexmlhttprequest.cpp \
     $$PWD/qdeclarativesqldatabase.cpp \
-    $$PWD/qmetaobjectbuilder.cpp \
     $$PWD/qdeclarativewatcher.cpp \
     $$PWD/qdeclarativecleanup.cpp \
     $$PWD/qdeclarativepropertycache.cpp \
@@ -106,7 +105,6 @@ HEADERS += \
     $$PWD/qdeclarativefastproperties_p.h \
     $$PWD/qdeclarativexmlhttprequest_p.h \
     $$PWD/qdeclarativesqldatabase_p.h \
-    $$PWD/qmetaobjectbuilder_p.h \
     $$PWD/qdeclarativewatcher_p.h \
     $$PWD/qdeclarativecleanup_p.h \
     $$PWD/qdeclarativepropertycache_p.h \

tests/auto/declarative/declarative.pro
@@ -1,8 +1,7 @@
 TEMPLATE = subdirs
 !symbian: {
 SUBDIRS += \
-           qdeclarativemetatype \
-           qmetaobjectbuilder
+           qdeclarativemetatype
 }
 
 SUBDIRS += \

commit 1f3d0ae63b2501460fc1c75f22b579b29147a80d

Fix regression from Qt 4 for deep group properties
Setting a property like someObject.font.family would cause an out-of-bounds
assertion in QList, called from QQmlCompiler::genValueTypeProperty. This
appears to be a regression from commit
5e970b84663f5398eb51d4575856d1a3c44df953, which replaced one -1 to
QMetaType::QVariant too many times. It appears the use of -1 is rather
deliberate here and not to indicate the use of a QVariant property.

The attached test verifies this as well as the successful setting of the
property at the end.

Task-number: QTBUG-31576
Change-Id: I237ea08847e1db31481a311ea8ec23a5ccc702d8
Reviewed-by: Matthew Vogt <matthew.vogt@qinetic.com.au>
Reviewed-by: Andrew den Exter <andrew.den.exter@qinetic.com.au>

src/declarative/qml/qdeclarativecompiler.cpp
@@ -1147,7 +1147,7 @@ void QDeclarativeCompiler::genValueTypeProperty(QDeclarativeParser::Object *obj,
     fetch.fetchValue.bindingSkipList = 0;
     fetch.line = prop->location.start.line;
 
-    if (obj->type == QMetaType::QVariant || output->types.at(obj->type).component) {
+    if (obj->type == -1 || output->types.at(obj->type).component) {
         // We only have to do this if this is a composite type.  If it is a builtin
         // type it can't possibly already have bindings that need to be cleared.
         foreach(Property *vprop, prop->value->valueProperties) {

tests/auto/declarative/qdeclarativelanguage/data/DeepComponent.qml
@@ -1,0 +1,7 @@
+import QtQuick 1.1
+Item {
+    property alias someObject: text
+    Text {
+        id: text
+    }
+}

tests/auto/declarative/qdeclarativelanguage/data/deepProperty.qml
@@ -1,0 +1,4 @@
+import QtQuick 1.1
+DeepComponent {
+    someObject.font.family: "test"
+}

tests/auto/declarative/qdeclarativelanguage/tst_qdeclarativelanguage.cpp
@@ -162,6 +162,8 @@ private slots:
 
     void compatibilitySemicolon();
 
+    void deepProperty();
+
 private:
     QDeclarativeEngine engine;
     void testType(const QString& qml, const QString& type, const QString& error);
@@ -2057,6 +2059,16 @@ void tst_qdeclarativelanguage::implicitImportsLast()
     QObject* object2 = object->property("item").value<QObject*>();
     QVERIFY(object2 != 0);
     QCOMPARE(QString(object2->metaObject()->className()), QLatin1String("QDeclarativeRectangle"));
+}
+
+void tst_qdeclarativelanguage::deepProperty()
+{
+    QDeclarativeComponent component(&engine, testFileUrl("deepProperty.qml"));
+    VERIFY_ERRORS(0);
+    QObject *o = component.create();
+    QVERIFY(o != 0);
+    QFont font = qvariant_cast<QFont>(qvariant_cast<QObject*>(o->property("someObject"))->property("font"));
+    QCOMPARE(font.family(), QStringLiteral("test"));
 }
 
 QTEST_MAIN(tst_qdeclarativelanguage)