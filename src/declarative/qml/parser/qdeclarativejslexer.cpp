87b91f933735aa2b7c2295fb8d1cf0692ccf2422
Erik Verbruggen <erik.verbruggen@digia.com>
2015-02-27 13:59:17 +0100

    Use QByteArray::toDouble() instead of qstrtod.
    No need to use a secretly exported symbol.
    
    Change-Id: I1cba959a789164cd9f584e9ff2591b7f85ab9a52
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- src/declarative/qml/parser/qdeclarativejslexer.cpp
+++ src/declarative/qml/parser/qdeclarativejslexer.cpp
@@ -48,10 +48,6 @@
 #include <stdio.h>
 #include <string.h>
 
-QT_BEGIN_NAMESPACE
-Q_CORE_EXPORT double qstrtod(const char *s00, char const **se, bool *ok);
-QT_END_NAMESPACE
-
 QT_QML_BEGIN_NAMESPACE
 
 #define shiftWindowsLineBreak() \
@@ -819,7 +815,7 @@ int Lexer::lex()
 
     double dval = 0;
     if (state == Number) {
-        dval = qstrtod(buffer8, 0, 0);
+        dval = QByteArray::fromRawData(buffer8, pos8).toDouble();
     } else if (state == Hex) { // scan hex numbers
         dval = integerFromString(buffer8, pos8, 16);
         state = Number;
